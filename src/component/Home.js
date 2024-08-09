df9677fd86caaf0fd0fe9c24a82fbce9e6bc96f8
Hardik-Vanani <vananihardik8553@gmail.com>
2024-08-09 13:06:13 +0530

    UI updates

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import Dashboard from "../pages/Dashboard";
 
 const Home = () => {
-  return (
-    <>
-      Home
-      {/* <Dashboard /> */}
-      {/* <Header />
+    return (
+        <>
+            Home
+            {/* <Dashboard /> */}
+            {/* <Header />
       <Sidebar /> */}
-    </>
-  );
+        </>
+    );
 };
 
 export default Home;
--- src/pages/Customer/index.js
+++ src/pages/Customer/index.js
@@ -8,77 +8,77 @@ import { faUserPen, faUserPlus, faUserSlash } from "@fortawesome/free-solid-svg-
 import "./CustomerList.css";
 
 const CustomerList = (props) => {
-  const [data, setData] = useState([]);
-  const URL = Url + "/customer";
+    const [data, setData] = useState([]);
+    const URL = Url + "/customer";
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(URL, config);
-      setData(response.data.payload.customerData);
-    } catch (error) {
-      console.log(error);
-      toast.error(error.response.data.message);
-    }
-  };
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(URL, config);
+            setData(response.data.payload.customerData);
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        }
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  const handleDelete = async (customerID) => {
-    try {
-      await axios.delete(`${URL}/${customerID}`, config);
-      fetchData();
-      toast.success("Customer Deleted Successfully");
-    } catch (error) {
-      return toast.error(error.response.data.message);
-    }
-  };
+    const handleDelete = async (customerID) => {
+        try {
+            await axios.delete(`${URL}/${customerID}`, config);
+            fetchData();
+            toast.success("Customer Deleted Successfully");
+        } catch (error) {
+            return toast.error(error.response.data.message);
+        }
+    };
 
-  return (
-    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-2 pb-2 border-bottom">
-        <h3 className="m-0">{props.name}</h3>
-        <div className="btn-toolbar mb-2 mb-md-0">
-          <NavLink to="add" className="btn btn-md btn-outline-dark">
-            Add Customer
-            <FontAwesomeIcon icon={faUserPlus} className="mx-2" />
-          </NavLink>
-        </div>
-      </div>
-
-      <div className="mt-3">
-        <table className="custom-table">
-          <thead>
-            <tr>
-              <th>Customer Name</th>
-              <th>Mobile No (+91)</th>
-              <th colSpan="2"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data &&
-              data.map((item) => (
-                <tr key={item._id}>
-                  <td>{item.customerName}</td>
-                  <td>{item.mobileNo}</td>
-                  <td className="action-icons">
-                    {" "}
-                    {/* Apply action icons class */}
-                    <NavLink to={{ pathname: `update/${item._id}` }} state={item} className="link-primary mx-2">
-                      <FontAwesomeIcon icon={faUserPen} />
+    return (
+        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-2 pb-2 border-bottom">
+                <h3 className="m-0">{props.name}</h3>
+                <div className="btn-toolbar mb-2 mb-md-0">
+                    <NavLink to="add" className="btn btn-md btn-outline-dark">
+                        Add Customer
+                        <FontAwesomeIcon icon={faUserPlus} className="mx-2" />
                     </NavLink>
-                    <div className="link-danger mx-2" onClick={() => handleDelete(item._id)}>
-                      <FontAwesomeIcon icon={faUserSlash} />
-                    </div>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-    </main>
-  );
+                </div>
+            </div>
+
+            <div className="mt-3">
+                <table className="custom-table">
+                    <thead>
+                        <tr>
+                            <th>Customer Name</th>
+                            <th>Mobile No (+91)</th>
+                            <th colSpan="2"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data &&
+                            data.map((item) => (
+                                <tr key={item._id}>
+                                    <td>{item.customerName}</td>
+                                    <td>{item.mobileNo}</td>
+                                    <td className="action-icons">
+                                        {" "}
+                                        {/* Apply action icons class */}
+                                        <NavLink to={{ pathname: `update/${item._id}` }} state={item} className="link-primary mx-2">
+                                            <FontAwesomeIcon icon={faUserPen} />
+                                        </NavLink>
+                                        <div className="link-danger mx-2" onClick={() => handleDelete(item._id)}>
+                                            <FontAwesomeIcon icon={faUserSlash} />
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
+        </main>
+    );
 };
 
 export default CustomerList;
